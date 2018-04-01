4f9d4ad0dad722ff7bc5d35c014879c4abca804b
KlimovAS <35884329+KlimovAS@users.noreply.github.com>
2018-04-01 16:52:07 +0300

    'bot01'

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# BrainQuizBot
 import config
 import telebot
 
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 # BrainQuizBot
+# BrainQuizBot
 token = '546969698:AAFnP1K7s0lHhJiE1Yvi6rB4YOOgVNc58TE'
